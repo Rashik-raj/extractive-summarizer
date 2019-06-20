23f924991d899e47a33a13f1178a5ed8f8bcc407
Rashik-raj <rashik123.rs@gmail.com>
2019-06-20 12:45:26 +0545

    some changes in UI and form validation added

---
+++ static/js/custom.js
@@ -0,0 +1,17 @@
+function validate()
+{
+    data=document.getElementById('textarea1').value;
+    errorMsg=document.getElementById('errorMsg');
+    flag = false;
+    if (data){
+        flag = true;
+    }
+    else{
+        errorMsg.style.display="block";
+        errorMsg.innerHTML="*** You must enter text or URL ***";
+    }
+    return flag;
+}
+$(document).ready(function(){
+    $('.modal').modal();
+});
